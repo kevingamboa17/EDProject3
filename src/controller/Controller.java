6b8d7c2ae40e6d4e0e4635824c3288176770303f
kevingamboa17 <kevin-gamboa1@hotmail.com>
2016-12-11 13:12:50 -0600

    Provisional

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package controller;
+
+import java.util.ArrayList;
+import model.Tweet;
+import persistence.Persistence;
+
 /**
  *
  * @author kevingamboa17
@@ -11,10 +16,25 @@ package controller;
 public class Controller {
 
     public Controller() {
+        
     }
     
+    public ArrayList<Tweet> getTweets(String fileName){
+        Persistence persistence = new Persistence();
+        return initProcess(persistence.getLocalTweets(fileName));
+    }
     
+    public String[] extractUsers(ArrayList<Tweet> array){
+        String[] userStrings= new String[array.size()];
+        for(int i=0; i<array.size();i++){
+            userStrings[i]= ((Tweet) array.get(i)).getUser().getScreen_name();
+        }
+        return userStrings;
+    }
     
-    
-    
+    public ArrayList<Tweet> initProcess(ArrayList<Tweet> arrayList){
+        Hash hash = new Hash();
+        
+        return null;
+    }
 }
