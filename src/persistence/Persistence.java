303a2728c6a34e751fed2f6f2c000a3c573d837c
kevingamboa17 <kevin-gamboa1@hotmail.com>
2016-12-12 08:02:08 -0600

    To fix 2

--- src/persistence/Persistence.java
+++ src/persistence/Persistence.java
@@ -25,11 +25,11 @@ public class Persistence {
         ArrayList<Tweet> arrayList = new ArrayList<>();
         try {
             Scanner sc = new Scanner(new File(fileName));
-            for(int i=0; i<1000; i++){
-              while(sc.hasNextLine()) {
-                arrayList.add(makeTweet(sc.nextLine()));
-              }  
-            }
+            
+            while(sc.hasNextLine()) {
+              arrayList.add(makeTweet(sc.nextLine()));
+            }  
+          
             sc.close();
         } 
         catch (Exception e) {
